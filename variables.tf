82136e1e01b33ad41a5e49c6bc766de3fd921add
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-14 14:03:04 +0100

    Add app_runner_connection_arn variable

--- variables.tf
+++ variables.tf
@@ -1,14 +1,28 @@
-variable "service_name" {
-  type    = string
-  default = "simple-url-shortener"
-}
-
-variable "repository_url" {
-  type    = string
-  default = "https://github.com/devgenix/simple-url-shortener"
-}
-
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "service_name" {
+  description = "App Runner service name"
+  type        = string
+  default     = "url-shortener"
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository URL"
+  type        = string
+  default     = "https://github.com/devgenix/simple-url-shortener"
+}
+
+variable "github_branch" {
+  description = "GitHub repository branch"
+  type        = string
+  default     = "main"
+}
+
+variable "app_runner_connection_arn" {
+  description = "The ARN of the existing App Runner GitHub connection"
+  type        = string
 }
