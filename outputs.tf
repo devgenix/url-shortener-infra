2f06de252b4b361a2db06ce634d5da2df49a23d7
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-14 14:15:03 +0100

    Fix undeclared resource references in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-output "service_url" {
-  value = aws_apprunner_service.this.service_url
+output "app_runner_service_url" {
+  description = "The URL of the App Runner service"
+  value       = aws_apprunner_service.url_shortener.service_url
 }
 
 output "connection_arn" {
-  value = aws_apprunner_connection.github.arn
-}
-
-output "connection_status" {
-  value = aws_apprunner_connection.github.status
+  description = "The ARN of the App Runner connection"
+  value       = var.app_runner_connection_arn
 }
