e5c7c82567aa3ddd52a04285457a189e128d61fd
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-14 14:03:04 +0100

    Use existing App Runner connection ARN instead of creating a new one

--- main.tf
+++ main.tf
@@ -1,6 +1,34 @@
-resource "aws_apprunner_connection" "github" {
-  connection_name = "github-connection"
-  provider_type   = "GITHUB"
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "apprunner_service_role" {
+  name = "${var.service_name}-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+      },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "tasks.apprunner.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner_service_role_policy" {
+  role       = aws_iam_role.apprunner_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
 resource "aws_apprunner_service" "this" {
@@ -8,13 +36,13 @@ resource "aws_apprunner_service" "this" {
 
   source_configuration {
     authentication_configuration {
-      connection_arn = aws_apprunner_connection.github.arn
+      connection_arn = var.app_runner_connection_arn
     }
     code_repository {
-      repository_url = var.repository_url
+      repository_url = var.github_repo_url
       source_code_version {
         type  = "BRANCH"
-        value = "main"
+        value = var.github_branch
       }
       code_configuration {
         configuration_source = "REPOSITORY"
@@ -22,9 +50,12 @@ resource "aws_apprunner_service" "this" {
     }
   }
 
-  network_configuration {
-    egress_configuration {
-      egress_type = "DEFAULT"
-    }
+  instance_configuration {
+    cpu    = "1024"
+    memory = "2048"
+  }
+
+  tags = {
+    Name = var.service_name
   }
 }
