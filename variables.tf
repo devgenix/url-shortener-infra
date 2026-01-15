fae3253ad5d68de267f360639976e4a712f304d9
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-15 12:02:40 +0100

    Add region variable and remove obsolete App Runner variable

--- variables.tf
+++ variables.tf
@@ -1,28 +1,5 @@
-variable "aws_region" {
+variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
-}
-
-variable "service_name" {
-  description = "App Runner service name"
-  type        = string
-  default     = "url-shortener"
-}
-
-variable "github_repo_url" {
-  description = "GitHub repository URL"
-  type        = string
-  default     = "https://github.com/devgenix/simple-url-shortener"
-}
-
-variable "github_branch" {
-  description = "GitHub repository branch"
-  type        = string
-  default     = "main"
-}
-
-variable "app_runner_connection_arn" {
-  description = "The ARN of the existing App Runner GitHub connection"
-  type        = string
 }
