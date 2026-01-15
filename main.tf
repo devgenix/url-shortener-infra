88992708a7f9c78b3322f5972396f1156f8b6dc4
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-15 12:02:40 +0100

    Add Redis ElastiCache to infrastructure

--- main.tf
+++ main.tf
@@ -2,47 +2,30 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
-
-locals {
-  name   = "url-shortener-eks"
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "url-shortener-vpc"
+  cidr = "10.0.0.0/16"
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = local.name
+  cluster_name    = "url-shortener-eks"
   cluster_version = "1.27"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     main = {
@@ -51,7 +34,6 @@ module "eks" {
       desired_size = 2
 
       instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
     }
   }
 }
@@ -60,4 +42,33 @@ resource "aws_ecr_repository" "app" {
   name                 = "eks-url-shortener"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "url-shortener-redis"
+  engine               = "redis"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.0"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name
+  security_group_ids   = [aws_security_group.redis.id]
+}
+
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "url-shortener-redis-subnet"
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_security_group" "redis" {
+  name        = "url-shortener-redis-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
 }
