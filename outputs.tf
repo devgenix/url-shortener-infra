6fd8591c5fedfdccbf43e19b27caa119bccf90bf
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-15 11:38:51 +0100

    Update outputs for EKS and ECR infrastructure

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
-output "app_runner_service_url" {
-  description = "The URL of the App Runner service"
-  value       = aws_apprunner_service.this.service_url
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
 }
 
-output "service_id" {
-  description = "The ID of the App Runner service"
-  value       = aws_apprunner_service.this.service_id
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
